3224dd3ec5c31f7f0269634b369ba5f543235d59
Brian Rosner <brosner@gmail.com>
2012-06-07 17:02:45 -0600

    added THEME_ACCOUNT_CONTACT_EMAIL in place of relying on d-u-a

--- pinax_theme_bootstrap_account/conf.py
+++ pinax_theme_bootstrap_account/conf.py
@@ -6,6 +6,7 @@ from appconf import AppConf
 class ThemeAccountAppConf(AppConf):
     
     ADMIN_URL = "admin:index"
+    CONTACT_EMAIL = "support@example.com"
     
     class Meta:
         prefix = "theme_account"
